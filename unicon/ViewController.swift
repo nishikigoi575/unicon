3ec277fef56cac0419770f7722d6b33252e63496
nowperson <imajinkawabe@gmail.com>
2018-06-16 16:15:37 +0900

    Initial Commit

---
+++ unicon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  unicon
+//
+//  Created by Imajin Kawabe on 2018/06/16.
+//  Copyright © 2018年 Imajin Kawabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
