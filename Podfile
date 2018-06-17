452b96daef3a10134c750aeefa65765148917f02
nowperson <imajinkawabe@gmail.com>
2018-06-17 13:31:03 +0900

    Installed Firebase and Facebook SDK

---
+++ Podfile
@@ -0,0 +1,29 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'unicon' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for unicon
+
+   pod 'Firebase/Core'
+   pod 'Firebase/Auth'
+   pod 'Firebase/Firestore'
+   
+   # Facebook
+   pod 'FBSDKCoreKit'
+   pod 'FBSDKLoginKit'
+   pod 'FBSDKShareKit'
+
+  target 'uniconTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'uniconUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- unicon/AppDelegate.swift
+++ unicon/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
@@ -39,6 +43,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        let handled:Bool = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
+        return handled
     }
 
 
