d4eb76fd6069c598ffea48732fb1123c3fe950af
Yo Hanashima <yohanashima@gmail.com>
2018-06-19 17:17:52 +0900

    test

--- unicon/AppDelegate.swift
+++ unicon/AppDelegate.swift
@@ -16,6 +16,8 @@ import IQKeyboardManagerSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, LoginButtonDelegate {
 
+    // test
+    
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
