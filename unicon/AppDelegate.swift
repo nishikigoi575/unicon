cd3434d3092ed3d3992bbb150d486b7401e20d12
nowperson <imajinkawabe@gmail.com>
2018-07-11 13:19:12 +0900

    ADDED TAPTIC ENGINE

--- unicon/AppDelegate.swift
+++ unicon/AppDelegate.swift
@@ -17,6 +17,10 @@ import IQKeyboardManagerSwift
 class AppDelegate: UIResponder, UIApplicationDelegate, LoginButtonDelegate {
     
     var window: UIWindow?
+    
+    // For ActivityIndicator
+    var actIdc = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+    var container: UIView?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -130,5 +134,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate, LoginButtonDelegate {
     }
 
 
+}
+
+extension AppDelegate {
+    // Activity Indicator
+    class func instance() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    func showActivityIndicator() {
+        if let window = window {
+            container = UIView()
+            container?.frame = window.frame
+            container?.center = window.center
+            container?.backgroundColor = UIColor(white: 0, alpha: 0.5)
+            
+            actIdc.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+            actIdc.hidesWhenStopped = true
+            actIdc.center = CGPoint(x: (container?.frame.size.width)! / 2, y: (container?.frame.size.height)! / 2)
+            
+            container?.addSubview(actIdc)
+            window.addSubview(container!)
+            
+            actIdc.startAnimating()
+        }
+    }
+    
+    func dismissActivityIndicator() {
+        if let _ = window, let container = container {
+            container.removeFromSuperview()
+        }
+    }
 }
 
