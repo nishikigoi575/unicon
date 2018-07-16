5f1bef9d6168e99e389fa620c0c8cf755b7c3c0f
nowperson <imajinkawabe@gmail.com>
2018-07-16 09:35:44 +0900

    no message

--- unicon/Views/MatchedView.swift
+++ unicon/Views/MatchedView.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+class MatchedView: UIView {
+    
+    @IBOutlet weak var myTeamImage: UIImageView!
+    @IBOutlet weak var teamImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var moveBtn: UIButton!
+    @IBOutlet weak var continueBtn: UIButton!
+    
+    @IBAction func moveToGroupChat(_ sender: Any) {
+    }
+    
+    @IBAction func `continue`(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    
+    override func awakeFromNib() {
+        teamImageView.layer.cornerRadius = teamImageView.frame.size.width / 2
+        myTeamImage.layer.cornerRadius = myTeamImage.frame.size.width / 2
+        teamImageView.layer.masksToBounds = true
+        myTeamImage.layer.masksToBounds = true
+    }
+    
+    
+    
+}
